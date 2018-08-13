ccf0c65e66e85857a239162460d09ae73c978e29
Kevin Abraham <abraha2d@users.noreply.github.com>
2018-08-13 17:49:48 -0400

    Update docs

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,13 +1,13 @@
 define({
   name: "coursewatch-backend",
-  version: "0.2.0-alpha.2",
+  version: "0.2.1-beta.1",
   description: "",
   sampleUrl: false,
   defaultVersion: "0.0.0",
   apidoc: "0.3.0",
   generator: {
     name: "apidoc",
-    time: "2018-08-10T04:07:05.831Z",
+    time: "2018-08-13T21:48:19.702Z",
     url: "http://apidocjs.com",
     version: "0.17.6"
   }
